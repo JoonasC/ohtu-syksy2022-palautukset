37ae1ae95506c9c2f63b6e89bdd24ddea7c0d19d
Joonas Coatanea <jcoatanea@protonmail.com>
2022-11-28 23:40:34 +0200

    Completed eleventh test

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -74,3 +74,10 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(leipa)
 
         self.assertEqual(len(self.kori.ostokset()), 2)
+
+    def test_kahden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostos(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+
+        self.assertEqual(len(self.kori.ostokset()), 1)
