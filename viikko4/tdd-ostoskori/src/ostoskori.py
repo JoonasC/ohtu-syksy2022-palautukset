ea92a93f543ba9323125f702c3bc8559b7619ae4
Joonas Coatanea <jcoatanea@protonmail.com>
2022-11-28 23:31:48 +0200

    Completed eighth test

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -4,21 +4,21 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        self.ostokset = []
+        self._ostokset = []
 
     def tavaroita_korissa(self):
-        return reduce(lambda acc, ostos: (acc + ostos.lukumaara()), self.ostokset, 0)
+        return reduce(lambda acc, ostos: (acc + ostos.lukumaara()), self._ostokset, 0)
 
     def hinta(self):
-        return reduce(lambda acc, ostos: (acc + ostos.hinta()), self.ostokset, 0)
+        return reduce(lambda acc, ostos: (acc + ostos.hinta()), self._ostokset, 0)
 
     def lisaa_tuote(self, lisattava: Tuote):
-        olemassaoleva_ostos = next((ostos for ostos in self.ostokset if ostos.tuotteen_nimi() == lisattava.nimi()), None)
+        olemassaoleva_ostos = next((ostos for ostos in self._ostokset if ostos.tuotteen_nimi() == lisattava.nimi()), None)
 
         if olemassaoleva_ostos:
             olemassaoleva_ostos.muuta_lukumaaraa(1)
         else:
-            self.ostokset.append(Ostos(lisattava))
+            self._ostokset.append(Ostos(lisattava))
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
@@ -29,6 +29,4 @@ class Ostoskori:
         # tyhjentää ostoskorin
 
     def ostokset(self):
-        pass
-        # palauttaa listan jossa on korissa olevat ostos-oliot
-        # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
+        return self._ostokset
